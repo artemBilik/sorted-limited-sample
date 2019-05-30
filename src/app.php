0de350b901b9d53639ac4293abefd182dbdf63cf
Artem Bilik <artem.bilik@gmail.com>
2019-05-30 13:39:20 +0300

    format code

--- src/app.php
+++ src/app.php
@@ -12,9 +12,9 @@ use Travelata\ArtemBilik\Samples\BulkSortSampleFactory;
 const THREADS_NUMBER = 16;
 
 require_once __DIR__.'/AutoLoader.php';
-
 (new AutoLoader())->register();
 $start = microtime(true);
+
 $sampleFactory = new BulkSortSampleFactory(1000, 20);
 $sample = $sampleFactory->create();
 $fileProvider = new ListFileProvider();
@@ -23,13 +23,14 @@ foreach (new DirectoryIterator(sprintf('%s/../files/', __DIR__)) as $file) {
         $fileProvider->addFile($file->getPathname());
     }
 }
+
 $pool = new Pool(THREADS_NUMBER, SampleWorker::class, [$sample, $fileProvider, $sampleFactory, new AutoLoader()]);
 for ($i = 0; $i < THREADS_NUMBER; $i++) {
     $pool->submit(new SampleTask());
 }
 $pool->shutdown();
-$result = new SplFileObject(__DIR__.'/../result.csv', 'w');
 
+$result = new SplFileObject(__DIR__.'/../result.csv', 'w');
 $csv = array_map(function (Product $product) {
     return $product->toCsv();
 }, $sample->getProducts());
